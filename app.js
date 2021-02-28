8a0d1fe041533836f6293c242a715e96740f8cab
Martijn Poppen <martijn.poppen@kpn.com>
2021-02-28 21:09:45 +0100

    OPT: V5 first driver

--- app.js
+++ app.js
@@ -1,5 +1,29 @@
 "use strict";
 
-module.exports.init = function() {
-	Homey.log("com.visionsecurity running...");
+const Homey = require("homey");
+
+class App extends Homey.App {
+  log() {
+    console.log.bind(this, "[log]").apply(this, arguments);
+
+    if (this.appSettings && this.appSettings.SET_DEBUG) {
+      return log.info.apply(log, arguments);
+    }
+  }
+
+  error() {
+    console.error.bind(this, "[error]").apply(this, arguments);
+
+    if (this.appSettings && this.appSettings.SET_DEBUG) {
+      return log.info.apply(log, arguments);
+    }
+  }
+
+  // -------------------- INIT ----------------------
+
+  async onInit() {
+    this.log(`${Homey.manifest.id} - ${Homey.manifest.version} started...`);
+  }
 }
+
+module.exports = App;
--- drivers/ZM1602/driver.js
+++ drivers/ZM1602/driver.js
@@ -1,68 +1,60 @@
-'use strict';
+"use strict";
 
-const path = require('path');
-const ZwaveDriver = require('homey-zwavedriver');
+const { ManagerFlows, ManagerDrivers } = require("homey");
+const { ZwaveDevice } = require("homey-zwavedriver");
 
 // Vision Security ZM1602 DC/AC Power Siren
 // http://www.pepper1.net/zwavedb/device/525
 
-module.exports = new ZwaveDriver(path.basename(__dirname), {
-	debug: false,
-	capabilities: {
-		'onoff': {
-			'command_class': 'COMMAND_CLASS_SWITCH_BINARY',
-			'command_get': 'SWITCH_BINARY_GET',
-			'command_set': 'SWITCH_BINARY_SET',
-			'command_set_parser': value => {
-				return {
-					'Switch Value': (value > 0) ? 255 : 0
-				};
-			},
-			'command_report': 'SWITCH_BINARY_REPORT',
-			'command_report_parser': report => report['Value'] === 'on/enable'
-		}
-	},
-	settings: {
-		'siren_strobe_mode': {
-			index: 1,
-			size: 1
-		},
-		'alarm_auto_stop': {
-			index: 2,
-			size: 1
-		}
-	}
-});
+class ZM1602 extends ZwaveDevice {
+  // this method is called when the Device is inited
+  async onNodeInit({ node }) {
+    // enable debugging
+    this.enableDebug();
 
-Homey.manager('flow').on('action.turn_alarm_on', function( callback, args ){
-        Homey.log('');
-        Homey.log('on flow action.action.turn_alarm_on');
-        Homey.log('args', args);
+    // print the node's info to the console
+    this.printNode();
 
-        Homey.manager('drivers').getDriver('ZM1602').capabilities.onoff.set(args.device, true, function (err, data) {
-                Homey.log('');
-                Homey.log('Homey.manager(drivers).getDriver(ZM1602).capabilities.onoff.set');
-                Homey.log('err', err);
-                Homey.log('data', data);
-                if (err) callback (err, false);
-        });
+    this.registerCapability("onoff", "COMMAND_CLASS_SWITCH_BINARY", {
+      get: "SWITCH_BINARY_GET",
+      set: "SWITCH_BINARY_SET",
+      setParser: (value) => {
+        return {
+          "Switch Value": value > 0 ? 255 : 0,
+        };
+      },
+      report: "SWITCH_BINARY_REPORT",
+      reportParser: (report) => report["Value"] === "on/enable",
+    });
 
-        callback( null, true );
-});
+    //   siren_strobe_mode
+    await this.configurationSet({ index: 1, size: 1 }, 10);
+    //   alarm_auto_stop
+    await this.configurationSet({ index: 2, size: 1 }, 10);
 
-Homey.manager('flow').on('action.turn_alarm_off', function( callback, args ){
-        Homey.log('');
-        Homey.log('on flow action.action.turn_alarm_on');
-        Homey.log('args', args);
+    
+    ManagerFlows.on("action.turn_alarm_on", function (callback, args) {
+      ManagerDrivers.getDriver("ZM1602").capabilities.onoff.set(
+        args.device,
+        true,
+        function (err, data) {
+          if (err) callback(err, false);
+        }
+      );
 
-        Homey.manager('drivers').getDriver('ZM1602').capabilities.onoff.set(args.device, false, function (err, data) {
-                Homey.log('');
-                Homey.log('Homey.manager(drivers).getDriver(ZM1602).capabilities.onoff.set');
-                Homey.log('err', err);
-                Homey.log('data', data);
-                if (err) callback (err, false);
-        });
+      callback(null, true);
+    });
 
-        callback( null, true );
-});
+    ManagerFlows.on("action.turn_alarm_off", function (callback, args) {
+      ManagerDrivers.getDriver("ZM1602").capabilities.onoff.set(
+        args.device,
+        false,
+        function (err, data) {
+          if (err) callback(err, false);
+        }
+      );
 
+      callback(null, true);
+    });
+  }
+}
