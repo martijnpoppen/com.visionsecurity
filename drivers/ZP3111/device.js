e40665656f98ab6c0b1439f555dad93a306e4037
Martijn Poppen <martijn.poppen@kpn.com>
2021-03-04 00:13:22 +0100

    FIX: ZP3111 humidity/temperature/luminance reporters

--- drivers/ZP3111/device.js
+++ drivers/ZP3111/device.js
@@ -14,108 +14,64 @@ class ZP3111 extends ZwaveDevice {
     this.printNode();
 
     this.registerCapability("alarm_motion", "NOTIFICATION", {
-        getParser: () => {
-            return {
-              "V1 Alarm Type": 0,
-              "Notification Type": "Access Control",
-              Event: 0,
-            };
-          },
-        report: "NOTIFICATION_REPORT",
-        reportParser: ( report ) => {
-            if (report['Event (Parsed)'] === 'Motion Detection, Unknown Location') {
-                return true;
-            }
-            
-            if (report['Event (Parsed)'] === 'Event inactive') {
-                return false;
-            }
-            
-            return null;
+      getParser: () => {
+        return {
+          "V1 Alarm Type": 0,
+          "Notification Type": "Access Control",
+          Event: 0,
+        };
+      },
+      report: "NOTIFICATION_REPORT",
+      reportParser: (report) => {
+        if (report["Event (Parsed)"] === "Motion Detection, Unknown Location") {
+          return true;
         }
-      });
 
-      this.registerCapability("alarm_tamper", "NOTIFICATION", {
-        optional: true,
-        getParser: () => {
-          return {
-            "V1 Alarm Type": 0,
-            "Notification Type": "Access Control",
-            Event: 0,
-          };
-        },
-        report: "NOTIFICATION_REPORT",
-        reportParser: report =>
-          report["Event (Parsed)"] === "Tampering, Product covering removed",
-      });
-
-
-
-	  this.registerCapability(
-        "measure_temperature",
-        "SENSOR_MULTILEVEL",
-        {
-          get: "SENSOR_MULTILEVEL_GET",
-          getParser: () => {
-            return {
-              "Sensor Type": "Temperature (version 1)",
-              Properties1: {
-                Scale: 0,
-              },
-            };
-          },
-          report: "SENSOR_MULTILEVEL_REPORT",
-          reportParser: report =>
-            report["Sensor Type"] !== "Temperature (version 1)"
-              ? null
-              : report["Sensor Value (Parsed)"],
+        if (report["Event (Parsed)"] === "Event inactive") {
+          return false;
         }
-      );
 
-      this.registerCapability(
-        "measure_luminance",
-        "SENSOR_MULTILEVEL",
-        {
-          get: "SENSOR_MULTILEVEL_GET",
-          getParser: () => {
-            return {
-              "Sensor Type": "Luminance (version 1)",
-              Properties1: {
-                Scale: 0,
-              },
-            };
+        return null;
+      },
+    });
+
+    this.registerCapability("alarm_tamper", "NOTIFICATION", {
+      getOpts: {
+        getOnOnline: true,
+      },
+      getParser: () => ({
+        "V1 Alarm Type": 0,
+        "Notification Type": "Access Control",
+        Event: 0,
+      }),
+      report: "NOTIFICATION_REPORT",
+      reportParser: (report) =>
+        report["Event (Parsed)"] === "Tampering, Product covering removed",
+    });
+
+    this.registerCapability("measure_temperature", "SENSOR_MULTILEVEL");
+
+    this.registerCapability("measure_luminance", "SENSOR_MULTILEVEL", {
+      get: "SENSOR_MULTILEVEL_GET",
+      getParser: () => {
+        return {
+          "Sensor Type": "Luminance (version 1)",
+          Properties1: {
+            Scale: 0,
           },
-          report: "SENSOR_MULTILEVEL_REPORT",
-          reportParser: report =>
-            report["Sensor Type"] !== "Luminance (version 1)"
-              ? null
-              : report["Sensor Value (Parsed)"],
-        }
-      );
+        };
+      },
+      report: "SENSOR_MULTILEVEL_REPORT",
+      reportParser: (report) =>
+        report["Sensor Type"] !== "Luminance (version 1)"
+          ? null
+          : report["Sensor Value (Parsed)"],
+    });
 
-      this.registerCapability(
-        "measure_humidity",
-        "SENSOR_MULTILEVEL",
-        {
-          get: "SENSOR_MULTILEVEL_GET",
-          getParser: () => {
-            return {
-              "Sensor Type": "Relative humidity (version 2)",
-              Properties1: {
-                Scale: 0,
-              },
-            };
-          },
-          report: "SENSOR_MULTILEVEL_REPORT",
-          reportParser: report =>
-            report["Sensor Type"] !== "Relative humidity (version 2)"
-              ? null
-              : report["Sensor Value (Parsed)"],
-        }
-      );
+    this.registerCapability("measure_humidity", "SENSOR_MULTILEVEL");
 
-      this.registerCapability("measure_battery", "BATTERY");
-	}
+    this.registerCapability("measure_battery", "BATTERY");
+  }
 }
 
 module.exports = ZP3111;
