3d655c28eef438b0c58a72ba4fa22763365b7f44
Martijn Poppen <m.poppen@ziggo.nl>
2021-12-24 14:32:28 +0100

    FIX: missing Capability

--- drivers/ZD2102/device.js
+++ drivers/ZD2102/device.js
@@ -1,11 +1,11 @@
 "use strict";
 
-const { ZwaveDevice } = require("homey-zwavedriver");
+const mainDevice = require("../main-device");
 
 // http://www.pepper1.net/zwavedb/device/702
 // http://www.pepper1.net/zwavedb/device/988
 
-class ZD2102 extends ZwaveDevice {
+class ZD2102 extends mainDevice {
   // this method is called when the Device is inited
   async onNodeInit({ node }) {
     const settings = this.getSettings();
@@ -15,23 +15,10 @@ class ZD2102 extends ZwaveDevice {
     // print the node's info to the console
     this.printNode();
 
+    await this.checkCapabilities();
+
     // technically you don't need both basic and alarm command classes as capability register, you could remove one of them
-    this.registerCapability("alarm_contact", "ALARM", {
-      report: "ALARM_REPORT",
-      reportParser: report => {
-        if (
-          report
-          && report.hasOwnProperty('Zwave Alarm Type')
-          && report['Zwave Alarm Type'] === 'Burglar'
-          && report.hasOwnProperty('ZWave Alarm Event')
-          && report['ZWave Alarm Event'] === 2
-          && report.hasOwnProperty('Alarm Level')
-        ) {
-          return (report['Alarm Level'] === 255);
-        }
-        return null;
-      }
-    });
+    this.registerCapability("alarm_contact", "BASIC");
 
     this.registerCapability("alarm_generic", "ALARM", {
       report: "ALARM_REPORT",
